c29f46a028922a8bcd9e188d92f2213315651a22
marcocnc <marcocancelliere56@gmail.com>
2023-06-07 14:17:04 +0200

    testing controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -8,5 +8,10 @@ use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $title = 'Home';
+        $description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt praesentium exercitationem nemo ut nostrum adipisci quia ex voluptatem placeat pariatur asperiores cupiditate tenetur temporibus incidunt aliquid magni ducimus architecto suscipit, quod harum qui. Maxime sint quidem rem nobis quisquam inventore dolorem corrupti incidunt facere. Sit mollitia voluptate numquam tenetur provident. Consequuntur, ipsum sapiente dignissimos perferendis velit laborum assumenda dicta dolorum nisi, odio ad asperiores cum, quibusdam doloribus repellendus. Quidem odit perferendis numquam animi repellat nostrum eligendi a ipsum perspiciatis, distinctio, consequatur modi dignissimos, facere alias quo? Aliquid quas, optio saepe a soluta voluptatem iste voluptas amet laboriosam velit fuga excepturi voluptatum id, unde voluptates ex et, expedita delectus dignissimos atque? Repudiandae harum, soluta quasi in explicabo enim laborum omnis aspernatur, tenetur eveniet eaque nemo iste nobis sit necessitatibus. Nihil alias harum magni ipsa perferendis explicabo vero dolorum quaerat facere inventore fugit officia ut cupiditate voluptates ducimus, qui voluptas ullam esse.';
+
+        return view('home', compact('title', 'description'));
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Model Controller</title>
+
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+
+        @extends('layouts.main')
+
+        @section('content')
+            <h2>altro test</h2>
+        @endsection
+
+    </body>
+</html>
